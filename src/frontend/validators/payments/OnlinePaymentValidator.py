1b855b72b05518f7966423190c7106717de29fc0
Alaa Omran <99070234+lolooppo@users.noreply.github.com>
2025-10-21 20:40:31 +0300

    Update OnlinePaymentValidator.py

--- src/frontend/validators/payments/OnlinePaymentValidator.py
+++ src/frontend/validators/payments/OnlinePaymentValidator.py
@@ -5,9 +5,10 @@ from src.frontend.validators.payments.IPaymentValidator import IPaymentValidator
 
 class OnlinePaymentValidator(IPaymentValidator):
 
-    def __inti__(self):
+    def __init__(self):
         self.GMAIL_REGEX = r"^[a-zA-Z0-9._%+-]+@gmail\.com$"
 
     def validate(self, payment: OnlinePayment):
         is_valid_email = bool(re.match(self.GMAIL_REGEX, payment.email))
+
         return is_valid_email
